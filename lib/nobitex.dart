8bece56998e36da610c6ed409c4793061c364ce5
Milad Nekofar <milad@nekofar.com>
2021-07-15 19:59:17 +0430

    feat: add new method to receive profile info using api

--- lib/nobitex.dart
+++ lib/nobitex.dart
@@ -1,7 +1,22 @@
 library nobitex;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Nobitex {
+  final String basePath;
+
+  final String token;
+
+  Nobitex({this.basePath = 'api.nobitex.ir', this.token = ''});
+
+  Future<Map<String, dynamic>?> getProfile() async {
+    var url = Uri.https(this.basePath, '/users/profile');
+
+    var response =
+        await http.post(url, headers: {'Authorization': 'Token ' + this.token});
+
+    return jsonDecode(response.body) as Map<String, dynamic>?;
+  }
 }
