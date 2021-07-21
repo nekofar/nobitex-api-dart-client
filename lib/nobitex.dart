2209a6f88bdbd1f65d65fa3f32e0bebf0189c454
Milad Nekofar <milad@nekofar.com>
2021-07-21 17:22:45 +0430

    refactor: remove return types to simplify codes
    Signed-off-by: Milad Nekofar <milad@nekofar.com>

--- lib/nobitex.dart
+++ lib/nobitex.dart
@@ -13,55 +13,53 @@ class Nobitex {
 
   /// Returns your profile information, bank card, bank account, verifications,
   /// profile settings and summary of your transaction statistics.
-  Future<Map<String, dynamic>?> getProfile() async {
+  getProfile() async {
     var url = Uri.https(basePath, '/users/profile');
 
     var response =
         await http.post(url, headers: {'Authorization': 'Token ' + token});
 
-    return jsonDecode(response.body) as Map<String, dynamic>?;
+    return jsonDecode(response.body);
   }
 
   /// Get a list of user wallets
-  Future<Map<String, dynamic>?> getWallets() async {
+  getWallets() async {
     var url = Uri.https(basePath, '/users/wallets/list');
 
     var response =
         await http.post(url, headers: {'Authorization': 'Token ' + token});
 
-    return jsonDecode(response.body) as Map<String, dynamic>?;
+    return jsonDecode(response.body);
   }
 
   /// Receive a list of deposits and withdrawals
-  Future<Map<String, dynamic>?> getWalletRecords() async {
+  getWalletRecords() async {
     var url = Uri.https(basePath, '/users/wallets/deposits/list');
 
     var response =
         await http.post(url, headers: {'Authorization': 'Token ' + token});
 
-    return jsonDecode(response.body) as Map<String, dynamic>?;
+    return jsonDecode(response.body);
   }
 
   /// Generate or receive a blockchain address
-  Future<Map<String, dynamic>?> getWalletAddress(
-      {required String wallet}) async {
+  getWalletAddress({required String wallet}) async {
     var url = Uri.https(basePath, '/users/wallets/generate-address');
 
     var response = await http.post(url,
         headers: {'Authorization': 'Token ' + token}, body: {'wallet': wallet});
 
-    return jsonDecode(response.body) as Map<String, dynamic>?;
+    return jsonDecode(response.body);
   }
 
   /// Receive the balance of fiat and cryptocurrency wallets
-  Future<Map<String, dynamic>?> getWalletBalance(
-      {required String currency}) async {
+  getWalletBalance({required String currency}) async {
     var url = Uri.https(basePath, '/users/wallets/balance');
 
     var response = await http.post(url,
         headers: {'Authorization': 'Token ' + token},
         body: {'currency': currency});
 
-    return jsonDecode(response.body) as Map<String, dynamic>?;
+    return jsonDecode(response.body);
   }
 }
