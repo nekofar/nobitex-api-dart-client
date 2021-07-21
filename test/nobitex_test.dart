47e8255bfb56f827608497f71f00644b1a64ea10
Milad Nekofar <milad@nekofar.com>
2021-07-22 01:48:33 +0430

    test: separate live and mock tests

--- test/nobitex_test.dart
+++ test/nobitex_test.dart
@@ -1,49 +1,7 @@
-import 'package:test/test.dart';
-import 'package:dotenv/dotenv.dart' show load, env;
-
-import 'package:nobitex/nobitex.dart';
+import 'nobitex_test_live.dart';
+import 'nobitex_test_mock.dart';
 
 void main() {
-  load();
-
-  final nobitex = Nobitex(basePath: 'testnetapi.nobitex.ir');
-
-  setUp(() async {
-    await nobitex.login(
-        username: env['NOBITEX_USERNAME'].toString(),
-        password: env['NOBITEX_PASSWORD'].toString());
-  });
-
-  group('tests for Nobitex', () {
-    test('test getProfile', () async {
-      var data = await nobitex.getProfile();
-
-      expect(data!.containsKey('profile'), true);
-    });
-
-    test('test getWallets', () async {
-      var data = await nobitex.getWallets();
-
-      expect(data!.containsKey('wallets'), true);
-    });
-
-    test('test getWalletRecords', () async {
-      var data = await nobitex.getWalletRecords();
-
-      expect(data!.containsKey('deposits'), true);
-    });
-
-    test('test getWalletAddress', () async {
-      var data = await nobitex.getWalletAddress(
-          wallet: env['NOBITEX_WALLET'].toString());
-
-      expect(data!.containsKey('address'), true);
-    });
-
-    test('test getWalletBalance', () async {
-      var data = await nobitex.getWalletBalance(currency: 'ltc');
-
-      expect(data!.containsKey('balance'), true);
-    });
-  });
+  testLive();
+  testMock();
 }
---
+++ test/nobitex_test_mock.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:test/test.dart';
+import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+import 'package:nobitex/nobitex.dart';
+
+void testMock() {
+  load();
+
+  final nobitex = Nobitex();
+
+  group('mock tests for Nobitex', () {
+    test('test getWalletAddress', () async {
+      nobitex.client = MockClient((request) async {
+        return Response(
+            json.encode({
+              "status": "ok",
+              "address": "LRf3vuTMy4UwD5b72G84hmkfGBQYJeTwUs"
+            }),
+            200,
+            headers: {'content-type': 'application/json'});
+      });
+
+      var data = await nobitex.getWalletAddress(wallet: '4159');
+
+      expect(data!.containsKey('address'), true);
+    });
+  });
+}
