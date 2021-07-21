ec8d3130832c1a0c6cc59c1ac2844c50930b5b40
Milad Nekofar <milad@nekofar.com>
2021-07-21 21:24:41 +0430

    feat: add a login method to fetch token by username and password

--- lib/nobitex.dart
+++ lib/nobitex.dart
@@ -6,11 +6,38 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class Nobitex {
-  final String basePath;
+  String basePath;
 
-  final String token;
+  String? token;
 
-  Nobitex({this.basePath = 'api.nobitex.ir', this.token = ''});
+  Nobitex({this.basePath = 'api.nobitex.ir', this.token});
+
+  login(
+      {required String username,
+      required String password,
+      String? totp}) async {
+    var url = Uri.https(basePath, '/auth/login/');
+
+    var response = await http.post(url, headers: {
+      'X-TOTP': totp ?? ''
+    }, body: {
+      'username': username,
+      'password': password,
+      'remember': 'yes',
+      'captcha': 'api'
+    });
+
+    var result = jsonDecode(response.body);
+    if (result['status'] == null) {
+      return null;
+    } else {
+      if (result['status'] == 'success') {
+        token = result['key'];
+      } else {
+        return null;
+      }
+    }
+  }
 
   /// Returns your profile information, bank card, bank account, verifications,
   /// profile settings and summary of your transaction statistics.
@@ -18,7 +45,7 @@ class Nobitex {
     var url = Uri.https(basePath, '/users/profile');
 
     var response = await http.post(url,
-        headers: {HttpHeaders.authorizationHeader: 'Token ' + token});
+        headers: {HttpHeaders.authorizationHeader: 'Token ' + token!});
 
     return jsonDecode(response.body);
   }
@@ -28,7 +55,7 @@ class Nobitex {
     var url = Uri.https(basePath, '/users/wallets/list');
 
     var response = await http.post(url,
-        headers: {HttpHeaders.authorizationHeader: 'Token ' + token});
+        headers: {HttpHeaders.authorizationHeader: 'Token ' + token!});
 
     return jsonDecode(response.body);
   }
@@ -38,7 +65,7 @@ class Nobitex {
     var url = Uri.https(basePath, '/users/wallets/deposits/list');
 
     var response = await http.post(url,
-        headers: {HttpHeaders.authorizationHeader: 'Token ' + token});
+        headers: {HttpHeaders.authorizationHeader: 'Token ' + token!});
 
     return jsonDecode(response.body);
   }
@@ -48,7 +75,7 @@ class Nobitex {
     var url = Uri.https(basePath, '/users/wallets/generate-address');
 
     var response = await http.post(url,
-        headers: {HttpHeaders.authorizationHeader: 'Token ' + token},
+        headers: {HttpHeaders.authorizationHeader: 'Token ' + token!},
         body: {'wallet': wallet});
 
     return jsonDecode(response.body);
@@ -59,7 +86,7 @@ class Nobitex {
     var url = Uri.https(basePath, '/users/wallets/balance');
 
     var response = await http.post(url,
-        headers: {HttpHeaders.authorizationHeader: 'Token ' + token},
+        headers: {HttpHeaders.authorizationHeader: 'Token ' + token!},
         body: {'currency': currency});
 
     return jsonDecode(response.body);
